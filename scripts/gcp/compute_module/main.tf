408065112c3ae3c00ff399be46aafab6f37007c3
Robert Teller <robert.j.c.teller@gmail.com>
2022-10-17 16:24:14 -0700

    update v2

--- scripts/gcp/compute_module/main.tf
+++ scripts/gcp/compute_module/main.tf
@@ -1,37 +1,56 @@
+resource "random_id" "id" {
+  byte_length = 2
+}
 
-resource "google_compute_instance" "instance-1" {
+module "gcp_utils" {
+  source  = "terraform-google-modules/utils/google"
+  version = "~> 0.3"
+}
+
+locals {
+  region_longname  = regex("projects/.*/regions/(?P<region>[^/]*)/*", var.subnetwork).region
+  region_shortname = module.gcp_utils.region_short_name_map[lower(local.region_longname)]
+  zone             = var.zone != null ? var.zone : data.google_compute_zones.available[0].names[0]
+}
+
+data "google_compute_zones" "available" {
+  count = var.zone == null ? 1 : 0
+
+  region  = local.region_longname
+  project = var.project_id
+}
+
+resource "google_compute_instance" "instance" {
   provider = google-beta
 
-  name         = "instance-1"
-  machine_type = "e2-micro"
-  zone         = "us-central1-a"
-  project      = "rteller-demo-svc-e265-aaaa"
+  name         = "cnpt-${local.region_shortname}-${random_id.id.hex}"
+  machine_type = var.machine_type
+  zone         = local.zone
+  project      = var.project_id
 
   metadata_startup_script = file("./metadata_startup_script")
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
-      type  = "pd-standard"
+      image = var.book_disk_image
+      type  = var.boot_disk_type
     }
   }
 
   dynamic "scratch_disk" {
-    # [] disable scratch disk / [1] enable scratch disk
-    for_each = []
+    for_each = var.scratch_disk ? [1] : []
     content {
       interface = "SCSI"
     }
   }
 
   network_interface {
-    subnetwork = "projects/rteller-demo-hst-e265-aaaa/regions/us-central1/subnetworks/default"
+    subnetwork = var.subnetwork
 
-    nic_type   = "GVNIC"
-    stack_type = "IPV4_ONLY"
+    nic_type   = var.nic_type
+    stack_type = var.ip_stack_type
     dynamic "access_config" {
-      # [] disable external ip / [1] enable external ip
-      for_each = []
+      for_each = var.external_ip ? [1] : []
       content {
         network_tier = "PREMIUM"
       }
@@ -39,7 +58,7 @@ resource "google_compute_instance" "instance-1" {
   }
 
   network_performance_config {
-    total_egress_bandwidth_tier = "DEFAULT"
+    total_egress_bandwidth_tier = var.egress_bandwidth_tier
   }
 
   scheduling {
--- scripts/gcp/main.tf
+++ scripts/gcp/main.tf
@@ -1,68 +1,18 @@
-resource "random_id" "id" {
-  byte_length = 2
+module "test" {
+  source     = "./compute_module"
+
+  subnetwork = var.subnetwork
+  project_id = var.project_id
 }
 
-module "gcp_utils" {
-  source  = "terraform-google-modules/utils/google"
-  version = "~> 0.3"
+variable "subnetwork" {
+  description = "Name of the subnetwork this instance should use to."
 }
 
-locals {
-  region_longname  = regex("projects/.*/regions/(?P<region>[^/]*)/*", var.subnetwork).region
-  region_shortname = module.gcp_utils.region_short_name_map[lower(local.region_longname)]
-  zone             = var.zone != null ? var.zone : data.google_compute_zones.available[0].names[0]
+variable "project_id" {
+  description = "Project id of the project that holds the network."
 }
 
-data "google_compute_zones" "available" {
-  count = var.zone == null ? 1 : 0
-
-  region  = local.region_longname
-  project = var.project_id
-}
-
-resource "google_compute_instance" "instance" {
-  provider = google-beta
-
-  name         = "cnpt-${local.region_shortname}-${random_id.id.hex}"
-  machine_type = var.machine_type
-  zone         = local.zone
-  project      = var.project_id
-
-  metadata_startup_script = file("./metadata_startup_script")
-
-  boot_disk {
-    initialize_params {
-      image = var.book_disk_image
-      type  = var.boot_disk_type
-    }
-  }
-
-  dynamic "scratch_disk" {
-    for_each = var.scratch_disk ? [1] : []
-    content {
-      interface = "SCSI"
-    }
-  }
-
-  network_interface {
-    subnetwork = var.subnetwork
-
-    nic_type   = var.nic_type
-    stack_type = var.ip_stack_type
-    dynamic "access_config" {
-      for_each = var.external_ip ? [1] : []
-      content {
-        network_tier = "PREMIUM"
-      }
-    }
-  }
-
-  network_performance_config {
-    total_egress_bandwidth_tier = var.egress_bandwidth_tier
-  }
-
-  scheduling {
-    provisioning_model = "STANDARD"
-
-  }
+output "instance_name" {
+  value = module.test.instance_name
 }
